6957f130361475f858827a273c08689094da3898
Jorge <jorge.bravo@live.u-tad.com>
2021-02-03 23:54:55 +0100

    actualizaciones

--- Trabajo-Colores.js
+++ Trabajo-Colores.js
@@ -5,62 +5,40 @@ var arrayColor = ["blue", "green", "black", "pink", "red", "orange"];
 var numColor = arrayColor.length;
 
 
-function RelojJS(){
+function mueveReloj(){
     momentoActual = new Date()
     hora = momentoActual.getHours()
     minuto = momentoActual.getMinutes()
     segundo = momentoActual.getSeconds()
 
-    str_segundo = new String (segundo)
-    if (str_segundo.length == 1)
-       segundo = "0" + segundo
-
-    str_minuto = new String (minuto)
-    if (str_minuto.length == 1)
-       minuto = "0" + minuto
-
-    str_hora = new String (hora)
-    if (str_hora.length == 1)
-       hora = "0" + hora
-
     horaImprimible = hora + " : " + minuto + " : " + segundo
 
     document.form_reloj.reloj.value = horaImprimible
 
-    setTimeout("RelojJS()",1000)
+    setTimeout("mueveReloj()",1000)
 }
 
 
-function getRandomColor() {
-   var letters = '0123456789ABCDEF';
-   var color = '#';
-   for (var i = 0; i < 6; i++) {
-     color += letters[Math.floor(Math.random() * 16)];
-   }
-   return color;
- }
- 
- 
- 
- function setRandomColor() {
-   $("#colorpad").css("background-color", getRandomColor());
- }
 
 
 
-// function cambioColor(indice){
+function cambioColor(x){
 
-//     var color = arrayColor[indice];
-
-//     var elementos = background-color+ arrayColor[indice]
-//     elementos.setAttribute('background-color',color);
-// }
-
-
-
-/*if(x.style.background=="#F7D358")
+  
+    if(x.style.background=="#F7D358")
     {
-        x.style.background="#fff";
-   }else{
+        x.style.background=="#a7D358"
+       
+   }else if(x.style.background=="#a7D358"){
+    x.style.background="#fff";
+   }else
+   {
         x.style.background="#F7D358";
-    }*/
+    }
+
+
+
+}
+
+
+
